2ec7c9beb2d4594c42030a7b3d396b12b51d6831
Brian McCall <mccabj0210@gmail.com>
2018-01-19 23:33:01 -0800

    add correct / incorrect buttons to both sides of card, vertically center notecard text

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,15 +1,19 @@
 <template>
 	<div class='quiz-card-container'>
 		<div class="notecard"  v-bind:class="{ flipped: showAnswer}">
-			<div class="score-buttons">
-				<button class="btn ripple btn-success score correct" v-on:click="markScore('correct')" :disabled="card.result === 'correct'"><i class="fa fa-check" aria-hidden="true"></i></button>
-				<button class="btn ripple btn-warning score incorrect" v-on:click="markScore('incorrect')" :disabled="card.result === 'incorrect'"><i class="fa fa-times" aria-hidden="true"></i></button>
-			</div>
 			<div class="front">
-				<p>{{card.answer}}</p>
+				<div class="score-buttons">
+					<button class="btn ripple btn-success score correct" v-on:click="markScore('correct')" :disabled="card.result === 'correct'"><i class="fa fa-check" aria-hidden="true"></i></button>
+					<button class="btn ripple btn-warning score incorrect" v-on:click="markScore('incorrect')" :disabled="card.result === 'incorrect'"><i class="fa fa-times" aria-hidden="true"></i></button>
+				</div>
+				<p>{{card.question}}</p>
 			</div>
 			<div class="back">
-				<p>{{card.question}}</p>
+				<div class="score-buttons">
+					<button class="btn ripple btn-success score correct" v-on:click="markScore('correct')" :disabled="card.result === 'correct'"><i class="fa fa-check" aria-hidden="true"></i></button>
+					<button class="btn ripple btn-warning score incorrect" v-on:click="markScore('incorrect')" :disabled="card.result === 'incorrect'"><i class="fa fa-times" aria-hidden="true"></i></button>
+				</div>
+				<p>{{card.answer}}</p>
 			</div>
 		</div>
 	</div>
@@ -26,13 +30,14 @@ export default {
 <style lang="css" scoped>
 
 @import "../styles/notecard.css";
+
 .score-buttons {
 	position: absolute;
-	top: 0px;
+	top: 0;
 	text-align: right;
 	padding-right: 25px;
 }
-.score-buttons >button {
+.score-buttons > button {
 		top: 50%;
 		margin-top: -10px;
 }
