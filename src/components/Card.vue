fd6d898816476c237a5e75b142c231890084e353
Brian McCall <mccabj0210@gmail.com>
2018-01-21 00:37:21 -0800

    hide answer during flipping

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -13,7 +13,7 @@
 					<button class="btn ripple btn-success score correct" v-on:click="markScore('correct')" :disabled="card.result === 'correct'"><i class="fa fa-check" aria-hidden="true"></i></button>
 					<button class="btn ripple btn-warning score incorrect" v-on:click="markScore('incorrect')" :disabled="card.result === 'incorrect'"><i class="fa fa-times" aria-hidden="true"></i></button>
 				</div>
-				<p>{{card.answer}}</p>
+				<p v-if="showAnswer">{{card.answer}}</p>
 			</div>
 		</div>
 	</div>
