561b4cb388a6bedcde3a8ca77d765d7fe56bfa9b
Brian McCall <mccabj0210@gmail.com>
2018-01-23 10:15:24 -0800

    add respective classes

--- src/components/Score.vue
+++ src/components/Score.vue
@@ -2,33 +2,18 @@
 	<div class="row score-box-row">
 		<div class="score-box col-xs-6">
 			<div class="text-center score-box-header">Correct</div>
-			<div class="text-center score-box-value">{{score.correct}}</div>
+			<div class="text-center score-box-value correct">{{score.correct}}</div>
 		</div>
 		<div class="score-box col-xs-6">
 			<div class="text-center score-box-header">Incorrect</div>
-			<div class="text-center score-box-value">{{score.incorrect}}</div>
+			<div class="text-center score-box-value incorrect">{{score.incorrect}}</div>
 		</div>
 	</div>
 </template>
 <script>
 export default {
 	name: 'Quiz',
-	props: ['score'],
-	data() {
-		return {
-			msg: 'Score',
-			deck: {
-				'name': 'Planets',
-				'questions': [
-					{'Planet closest to the sun': 'Mercury'},
-					{'Planet furthest away from the sun': 'Neptune'},
-					{'What is the largest planet': 'Jupiter'},
-					{'What is the hottest planet': 'Venus'},
-					{'What is the smallest planet': 'Mercury'}
-				]
-			}
-		}
-	}
+	props: ['score']
 }
 </script>
 <style scoped>
