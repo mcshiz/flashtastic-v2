1f3a6a56be47f3962b25259f1e701204816a32a1
Brian McCall <mccabj0210@gmail.com>
2018-01-26 21:03:29 -0800

    add user menu

---
+++ src/components/UserMenu.vue
@@ -0,0 +1,14 @@
+<template>
+	<button v-if="authenticated">Sign Out</button>
+	<button v-else>Sign In With Google</button>
+</template>
+<script>
+import { mapState } from 'vuex'
+
+export default {
+	name: 'Home',
+	computed: {
+		...mapState(['authenticated', 'user'])
+	}
+}
+</script>
