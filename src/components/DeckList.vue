b1f2c3c9d4dec4d07d5f5412f54cbf297101d087
Brian McCall <mccabj0210@gmail.com>
2018-01-27 09:58:55 -0800

    fix html markup

--- src/components/DeckList.vue
+++ src/components/DeckList.vue
@@ -4,16 +4,16 @@
 			<ul class="list-group">
 				<li v-for='(deck, index) in decks' :key='`deck-${index}`' class="list-group-item">
 					<div class="row">
-						<span class="col-12 col-md-10 text-left justify-content-center align-self-center">
+						<span class="col-6 col-sm-8 col-md-10 text-left justify-content-center align-self-center">
 							<b>{{deck.name}}</b>
 							<br>
 							{{deck.questions.length}} Cards
 						</span>
-						<div class="col-auto col-md-1 pull-right" >
+						<div class="col-4 col-sm-2 col-md-1 pull-right" >
 							<button class="btn btn-info" v-on:click="loadQuiz(index)">Study</button>
 						</div>
-						<div class="col-md-1">
-							<span class="delete-deck" v-on:click="deleteDecK(index)"><i class="fa fa-trash"></i></span>
+						<div class="col-2 col-sm-2 col-md-1">
+							<span class="delete-deck" v-on:click="deleteDeck(index)"><i class="fa fa-trash"></i></span>
 						</div>
 					</div>
 				</li>
@@ -29,15 +29,14 @@ export default {
 	methods: {
 		loadQuiz: function(id) {
 			this.$store.dispatch('LOAD_DECK_BY_ID', id).then((data) => {
-				console.log(this.$store.state.selectedDeck[0])
 				const selectedDeckName = encodeURI(this.$store.state.selectedDeck.name.replace(/\s/g, '-').toLowerCase())
 				this.$router.push({name: 'Quiz', params: {id: id, name: selectedDeckName}})
 			})
 		},
-		deleteDecK: function(id) {
-			this.$store.dispatch('DELETE_DECK_BY_ID', id).then((data) => {
-				console.log(this.$store.state.myDecks)
-			})
+		deleteDeck: function(id) {
+			if(window.confirm('Are you sure you want to delete this deck?')) {
+				this.$store.dispatch('DELETE_DECK_BY_ID', id)
+			}
 		}
 	}
 }
