a3a0973401d0a7168c38702b82f581d0ebc5c0c5
Brian McCall <mccabj0210@gmail.com>
2018-01-29 02:55:12 -0800

    add ability to use images for questions / answers

--- src/components/CreateNew.vue
+++ src/components/CreateNew.vue
@@ -13,14 +13,14 @@
 				</div>
 			</div>
 			<div class="row">
-				<div class="col">
+				<div class="col-12 col-sm-6">
 					<input type="text" class="form-control tags" placeholder="Chemistry, Nursing" id="deck-tags">
 					<small id="tagsHelp" class="form-text text-muted text-left">Tag this deck for easier finding later</small>
 				</div>
-				<div class="col">
+				<div class="col-12 col-sm-6">
 					<select name="deckPermissions" id="deckPermissions" v-model="deckPermissions" v-on:change="saveDeckPermissions">
-						<option value="private">Private - Only I can play this deck</option>
-						<option value="public">Public - Let everyone pay this deck</option>
+						<option value="private">Private - Only I can use this deck</option>
+						<option value="public">Public - Let everyone use this deck</option>
 					</select>
 				</div>
 			</div>
@@ -33,34 +33,47 @@
 				<div class="col-12">
 					<h4 class="text-left">Add a new question</h4>
 				</div>
-				<div class="col-12 col-sm-6">
-					<div class="input-group">
-						<input type="text" class="form-control" placeholder="Question" aria-label="Question" v-model="newQuestion">
+				<div class="col-12 col-sm-5 text-left">
+					<div v-bind:class="{'input-group' : newQuestionType === 'text'}">
+						<span v-if="questionImageLoading">Loading...</span>
+						<img v-if="newQuestionType === 'image'"  :src="newQuestion" alt="" height="200" width="200">
+						<input v-else type="text" class="form-control" placeholder="Question" aria-label="Question" v-model="newQuestion">
+						<div class="input-group-append">
+							<button class="btn btn-secondary" type="button" v-on:click="useQuestionImage" tabindex="-1"><i class="fa fa-camera"></i></button>
+						</div>
+						<div style="height:0;overflow:hidden;display:none">
+							<input type="file" id="QuestionFileInput" name="fileInput" v-on:change="uploadQuestionImage"/>
+						</div>
 					</div>
 				</div>
 				<div class="col-12 col-sm-5">
 					<div class="input-group">
-						<input type="text" class="form-control" placeholder="Answer" aria-label="Answer" v-model="newAnswer">
+						<span v-if="answerImageLoading">Loading...</span>
+						<img v-if="newAnswerType === 'image'"  :src="newAnswer" alt="" height="200" width="200">
+						<input v-else type="text" class="form-control" placeholder="Answer" aria-label="Answer" v-model="newAnswer">
+						<span v-bind:class="{'input-group-append' : newAnswerType === 'text'}" >
+							<button class="btn btn-secondary" type="button" v-on:click="useAnswerImage" tabindex="-1`"><i class="fa fa-camera"></i></button>
+						</span>
+						<div style="height:0;overflow:hidden;display:none">
+							<input type="file" id="AnswerFileInput" name="fileInput" v-on:change="uploadAnswerImage"/>
+						</div>
 					</div>
 				</div>
-				<div class="col-xs-12 col-sm-auto">
-					<button class="btn ripple btn-success" v-on:click="saveCard">Save</button>
+				<div class="col-12 col-sm-2">
+					<button class="btn ripple btn-success" v-on:click="addCard">Add</button>
+					<button class="btn ripple btn-secondary" v-on:click="clearCard">Clear</button>
 				</div>
 			</div>
 			<div class="row">
-				<div class="col text-left">
-					<button class="btn ripple btn-danger" v-on:click="resetNewDeck"><i class="fa fa-refresh" aria-hidden="true"></i>Reset</button>
+				<div class="col-12 justify-content-sm-between">
+					<button class="btn ripple btn-danger" v-on:click="resetNewDeck"><i class="fa fa-refresh" aria-hidden="true"></i>Clear All</button>
 					<button class="btn ripple btn-primary" v-on:click="createDeck">Create Deck</button>
-				</div>
-				<div class="col text-left">
-
 				</div>
 			</div>
 		</div>
 </template>
 
 <script>
-import NewQuestions from './NewQuestions'
 import NewCardsList from './NewCardsList'
 import NameDeckModal from './NameDeckModal'
 import $ from 'jquery'
@@ -72,12 +85,27 @@ export default {
 			msg: 'Create a new quiz deck',
 			newQuestion: '',
 			newAnswer: '',
+			newQuestionType: 'text',
+			newAnswerType: 'text',
 			deckPermissions: 'private',
-			editing: false
+			questionImageLoading: false,
+			answerImageLoading: false,
+			editing: false,
+			created: false
 		}
 	},
 	computed: {
-		...mapState(['newQuestions', 'newDeck'])
+		...mapState(['newDeck'])
+	},
+	beforeRouteLeave(to, from, next) {
+		if(!this.created) {
+			if(confirm('Leaving will discard any changes')) {
+				this.resetNewDeck().then(() => {
+					next()
+				})
+			}
+		}
+		next()
 	},
 	methods: {
 		deleteCard: function(index) {
@@ -90,24 +118,37 @@ export default {
 			$('#editModal').modal('hide')
 		},
 		resetNewDeck: function() {
-			this.newQuestion = ''
-			this.newAnswer = ''
-			this.deckPermissions = 'private'
-			this.$store.dispatch('RESET_NEW_DECK')
-			$('#nameDeckModal').modal()
+			return new Promise((resolve, reject) => {
+				this.newQuestion = ''
+				this.newAnswer = ''
+				this.newQuestionType = 'text'
+				this.newAnswerType = 'text'
+				this.deckPermissions = 'private'
+				this.$store.dispatch('RESET_NEW_DECK').then(() => {
+					resolve()
+				})
+			})
 		},
 		saveDeckName: function (name) {
-			this.$store.dispatch('NAME_DECK', name)
+			this.$store.dispatch('CREATE_NEW_DECK', name)
 		},
-		saveCard: function() {
-			if(this.newQuestion === '' || this.newAnswer === '') {
+		clearCard: function() {
+			this.newQuestion = ''
+			this.newAnswer = ''
+			this.newQuestionType = 'text'
+			this.newAnswerType = 'text'
+		},
+		addCard: function() {
+			if((this.newQuestion === '' && this.newQuestionType === 'text') || (this.newAnswer === '' && this.newAnswerType === 'text')) {
 				this.$store.dispatch('SHOW_ERROR', 'Both question and answer must be entered')
 				return
 			}
-			let newQuestion = {'question': this.newQuestion, 'answer': this.newAnswer}
+			let newQuestion = {'question': this.newQuestion, 'questionType': this.newQuestionType, 'answer': this.newAnswer, 'answerType': this.newAnswerType}
 			this.$store.dispatch('ADD_NEW_QUESTION', newQuestion)
 			this.newQuestion = ''
 			this.newAnswer = ''
+			this.newQuestionType = 'text'
+			this.newAnswerType = 'text'
 		},
 		saveDeckPermissions: function() {
 			this.$store.dispatch('CHANGE_DECK_PERMISSIONS', this.deckPermissions)
@@ -117,13 +158,52 @@ export default {
 				this.$store.dispatch('SHOW_ERROR', 'You must first add questions and answers')
 				return
 			}
+			if(this.newDeck.name === '') {
+				$('#nameDeckModal').modal({
+					keyboard: false,
+					backdrop: 'static'
+				})
+				return
+			}
 			this.$store.dispatch('CREATE_DECK').then(() => {
+				this.created = true
 				this.$router.push({path: 'home'})
 			})
+		},
+		useQuestionImage: function() {
+			document.getElementById('QuestionFileInput').click()
+		},
+		useAnswerImage: function() {
+			document.getElementById('AnswerFileInput').click()
+		},
+		uploadQuestionImage: function(e) {
+			let file = e.target.files[0]
+			let reader = new FileReader()
+			this.newQuestionType = 'image'
+			file.src = reader.result
+			this.questionImageLoading = true
+			this.$store.dispatch('UPLOAD_IMAGE', file).then(response => {
+				this.newQuestion = response.downloadURL
+				this.questionImageLoading = false
+			}).catch(error => [
+				console.log('err', error)
+			])
+		},
+		uploadAnswerImage: function(e) {
+			let file = e.target.files[0]
+			let reader = new FileReader()
+			this.newAnswerType = 'image'
+			file.src = reader.result
+			this.answerImageLoading = true
+			this.$store.dispatch('UPLOAD_IMAGE', file).then(response => {
+				this.newAnswer = response.downloadURL
+				this.answerImageLoading = false
+			}).catch(error => [
+				console.log('err', error)
+			])
 		}
 	},
 	components: {
-		'newQuestions': NewQuestions,
 		'newCardsList': NewCardsList,
 		'nameDeckModal': NameDeckModal
 	}
