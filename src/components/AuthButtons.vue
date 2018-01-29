c9063c038de4228a24ac79326317255cf821c040
Brian McCall <mccabj0210@gmail.com>
2018-01-29 02:54:00 -0800

    put auth buttons into own component

---
+++ src/components/AuthButtons.vue
@@ -0,0 +1,21 @@
+<template>
+	<button v-if="authenticated" v-on:click="signOut" class="btn btn-primary ripple">Sign Out</button>
+	<button v-else v-on:click="authenticate" class="btn btn-primary ripple">Sign In With Google</button>
+</template>
+<script>
+import {mapState} from 'vuex'
+export default {
+	name: 'AuthButtons',
+	computed: {
+		...mapState(['authenticated'])
+	},
+	methods: {
+		authenticate: function() {
+			this.$store.dispatch('SIGN_IN')
+		},
+		signOut: function() {
+			this.$store.dispatch('SIGN_OUT')
+		}
+	}
+}
+</script>
