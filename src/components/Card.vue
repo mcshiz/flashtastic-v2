97e6fdc6bc53e8205496dc4e9caa52fa944ff01b
Brian McCall <mccabj0210@gmail.com>
2018-01-29 02:54:23 -0800

    show images on cards

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -6,14 +6,16 @@
 					<button class="btn ripple btn-success score correct" v-on:click="markScore('correct')" :disabled="card.result === 'correct'"><i class="fa fa-check" aria-hidden="true"></i></button>
 					<button class="btn ripple btn-warning score incorrect" v-on:click="markScore('incorrect')" :disabled="card.result === 'incorrect'"><i class="fa fa-times" aria-hidden="true"></i></button>
 				</div>
-				<p>{{card.question}}</p>
+				<img v-if="card.questionType === 'image'" :src="card.question" alt="" class="card-image align-self-end">
+				<div v-else>{{card.question}}</div>
 			</div>
 			<div class="back">
 				<div class="score-buttons">
 					<button class="btn ripple btn-success score correct" v-on:click="markScore('correct')" :disabled="card.result === 'correct'"><i class="fa fa-check" aria-hidden="true"></i></button>
 					<button class="btn ripple btn-warning score incorrect" v-on:click="markScore('incorrect')" :disabled="card.result === 'incorrect'"><i class="fa fa-times" aria-hidden="true"></i></button>
 				</div>
-				<p v-if="showAnswer">{{card.answer}}</p>
+				<img v-if="card.answerType === 'image' && showAnswer" :src="card.answer" alt="" class="card-image align-self-end">
+				<div v-else-if="card.answerType === 'text' && showAnswer">{{card.answer}}</div>
 			</div>
 		</div>
 	</div>
@@ -40,5 +42,8 @@ export default {
 .score-buttons > button {
 		top: 50%;
 		margin-top: -10px;
+}
+.card-image {
+	height: 85%;
 }
 </style>
