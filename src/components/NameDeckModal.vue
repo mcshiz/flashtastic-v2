b3a3961a6950f46c874d3f6126c432d7bc3d9f1c
Brian McCall <mccabj0210@gmail.com>
2018-01-29 20:02:43 -0800

    change title of modal

--- src/components/NameDeckModal.vue
+++ src/components/NameDeckModal.vue
@@ -3,7 +3,7 @@
 		<div class="modal-dialog modal-dialog-centered" role="document">
 			<div class="modal-content">
 				<div class="modal-header">
-					<h5 class="modal-title">Name your Deck of Notecards</h5>
+					<h5 class="modal-title">Name This Deck of Notecards</h5>
 				</div>
 				<div class="modal-body">
 					<div class="input-group">
