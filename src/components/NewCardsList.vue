bbfda95b96aa3939c11c8c14015f1462da7930d3
Brian McCall <mccabj0210@gmail.com>
2018-01-30 00:18:57 -0800

    styling; moved save and delete functions into this component

--- src/components/NewCardsList.vue
+++ src/components/NewCardsList.vue
@@ -1,10 +1,10 @@
 <template>
-	<div>
-		<editCardModal :card="card" :index="editIndex" :save="save"></editCardModal>
+	<div class="col">
+		<editCardModal :card="card" :index="editIndex" :save="saveEdits"></editCardModal>
 		<ul class="list-group">
 			<li v-for='(newCard, index) in cards' :key='`question-${index}`' class="list-group-item">
 				<div class="row text-left">
-					<div class="col-12 col-sm-5 align-self-center">
+					<div class="col-12 col-sm-5 col-md-4 align-self-center">
 						<b class="new-question-label">Question:</b>
 						<span v-if="newCard.questionType === 'image'" >
 							<br>
@@ -12,7 +12,7 @@
 						</span>
 						<span v-else>{{newCard.question}}</span>
 					</div>
-					<div class="col-12 col-sm-5 align-self-center">
+					<div class="col-12 col-sm-5 col-md-4 align-self-center">
 						<b class="new-question-label">Answer:</b>
 						<span v-if="newCard.answerType === 'image'">
 							<br>
@@ -20,10 +20,10 @@
 						</span>
 						<span v-else>{{newCard.answer}}</span>
 					</div>
-					<div class="col-auto col-md-1 pull-right align-self-center">
+					<div class="col-auto col-sm-1 col-md-2 mt-2 text-right align-self-center">
 						<button class="btn ripple btn-primary" v-on:click="editCard(newCard, index)">Edit</button>
 					</div>
-					<div class="col-auto col-md-1 pull-right align-self-center">
+					<div class="col-auto col-sm-1  col-md-2 mt-2 pull-right align-self-center">
 						<button class="btn ripple btn-danger" v-on:click="deleteCard(index)">Delete</button>
 					</div>
 				</div>
@@ -37,7 +37,7 @@ import EditCardModal from './EditCardModal'
 import $ from 'jquery'
 export default {
 	name: 'newCardList',
-	props: ['cards', 'deleteCard', 'save'],
+	props: ['cards', 'save', 'delete'],
 	data() {
 		return {
 			card: {},
@@ -55,6 +55,15 @@ export default {
 			this.card = card
 			this.editIndex = index
 			$('#editModal').modal()
+		},
+		deleteCard: function(index) {
+			if(window.confirm('Are you sure you want to delete this question?')) {
+				this.delete(index)
+			}
+		},
+		saveEdits: function(card) {
+			this.save(card)
+			$('#editModal').modal('hide')
 		}
 	}
 }
