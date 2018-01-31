ed98ff5fffe0adc62cb402d0a1c4c9c19f8335f8
Brian McCall <mccabj0210@gmail.com>
2018-01-30 17:18:08 -0800

    remove placeholders for now

--- src/components/NewQuestionAnswerFields.vue
+++ src/components/NewQuestionAnswerFields.vue
@@ -2,7 +2,7 @@
 	<div v-bind:class="{'input-group' : type === 'text'}">
 		<span v-if="loading">Loading...</span>
 		<img v-if="type === 'image'"  :src="value" alt="" height="200" width="200" class="mb-2">
-		<input v-else type="text" class="form-control" :placeholder="field.replace(/image/i, '').toUpperCase()" aria-label="Question"  v-bind:value="value" v-on:input="updateValue($event.target.value)">
+		<input v-else type="text" class="form-control" aria-label="Question"  v-bind:value="value" v-on:input="updateValue($event.target.value)">
 		<div class="input-group-append justify-content-center">
 			<button class="btn btn-secondary" type="button" v-on:click="openImageFinder" tabindex="-1"><i class="fa fa-camera"></i></button>
 		</div>
